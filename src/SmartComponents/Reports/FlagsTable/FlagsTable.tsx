38e11c59ef0867f26dec596664314336f29c4960
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-09-05 09:55:57 +0200

    fix background table

--- src/SmartComponents/Reports/FlagsTable/FlagsTable.tsx
+++ src/SmartComponents/Reports/FlagsTable/FlagsTable.tsx
@@ -26,10 +26,7 @@ import {
     EmptyStateVariant,
     Title,
     TitleLevel,
-    EmptyStateBody,
-    Card,
-    CardBody
-} from '@patternfly/react-core';
+    EmptyStateBody} from '@patternfly/react-core';
 import { ErrorCircleOIcon, SearchIcon } from '@patternfly/react-icons';
 import { FlagModel } from '../../../models';
 import { ObjectFetchStatus } from '../../../models/state';
@@ -254,17 +251,15 @@ class FlagsTable extends React.Component<Props, State> {
     public renderNoResults = () => {
         return (
             <React.Fragment>
-                <Card>
-                    <CardBody>
-                        <EmptyState variant={ EmptyStateVariant.full }>
-                            <EmptyStateIcon icon={ SearchIcon } />
-                            <Title headingLevel="h5" size="lg">No results found</Title>
-                            <EmptyStateBody>
-                                No results match the search criteria
-                            </EmptyStateBody>
-                        </EmptyState>
-                    </CardBody>
-                </Card>
+                <Bullseye>
+                    <EmptyState variant={ EmptyStateVariant.full }>
+                        <EmptyStateIcon icon={ SearchIcon } />
+                        <Title headingLevel="h5" size="lg">No results found</Title>
+                        <EmptyStateBody>
+                            No results match the search criteria
+                        </EmptyStateBody>
+                    </EmptyState>
+                </Bullseye>
             </React.Fragment>
         );
     };
