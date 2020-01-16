72833ac9ad62ae91fc62222e32164e459c520ec7
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2020-01-16 10:59:20 +0100

    MIGENG-173: UI Upload Form: Default “Report Name” should not include the extension of the file (#72)
    * remove all the extensions from a a file
    
    * move extractFIlename to utils and add tests

---
+++ src/Utilities/extractUtils.test.tsx
@@ -0,0 +1,21 @@
+import { getFilenameWithoutExtensions } from './extractUtils';
+
+describe('Utils', () => {
+    it('expect to remove .zip extension', () => {
+        const filename = 'myFilename.zip';
+        const filenameWithoutExtension = getFilenameWithoutExtensions(filename);
+        expect(filenameWithoutExtension).toEqual('myFilename');
+    });
+
+    it('expect to remove .tar.gz extension', () => {
+        const filename = 'myFilename.tar.gz';
+        const filenameWithoutExtension = getFilenameWithoutExtensions(filename);
+        expect(filenameWithoutExtension).toEqual('myFilename');
+    });
+
+    it('expect to remove any extension', () => {
+        const filename = 'myFilename.extension1.extension2';
+        const filenameWithoutExtension = getFilenameWithoutExtensions(filename);
+        expect(filenameWithoutExtension).toEqual('myFilename');
+    });
+});
