15f796cab82c932b92b1445a42c2bb7918663c21
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2020-04-09 11:02:42 +0200

    MIGENG-553: MA - WSR - replace Container-Native Virtualization with OpenShift virtualization (#84)
    * replace Container-Native Virtualization with OpenShift virtualization
    
    * Update src/pages/ReportView/WorkloadSummary/WorkloadSummary.tsx
    
    Co-Authored-By: Marco Rizzi <mrizzi@users.noreply.github.com>
    
    * add tests
    
    Co-authored-by: Marco Rizzi <mrizzi@users.noreply.github.com>

---
+++ src/pages/ReportView/WorkloadSummary/WorkloadSummary.test.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { WorkloadMigrationSummary, WorkloadMigrationSummaryProps } from './WorkloadSummary';
+
+describe('WorkloadMigrationSummary', () => {
+    it('expect to render error', () => {
+        const props: WorkloadMigrationSummaryProps = {
+            reportId: 1,
+            reportWorkloadSummary: null,
+            reportWorkloadSummaryFetchStatus: { error: 'Error message', status: 'complete' },
+            fetchReportWorkloadSummary: (reportId: number) => ({
+                then: () => ({
+                    catch: jest.fn()
+                })
+            })
+        };
+        const wrapper = shallow(<WorkloadMigrationSummary {...props} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to render skeleton', () => {
+        const props: WorkloadMigrationSummaryProps = {
+            reportId: 1,
+            reportWorkloadSummary: null,
+            reportWorkloadSummaryFetchStatus: { error: null, status: 'inProgress' },
+            fetchReportWorkloadSummary: (reportId: number) => ({
+                then: () => ({
+                    catch: jest.fn()
+                })
+            })
+        };
+        const wrapper = shallow(<WorkloadMigrationSummary {...props} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to render reports', () => {
+        const props: WorkloadMigrationSummaryProps = {
+            reportId: 1,
+            reportWorkloadSummary: {
+                summaryModels: [
+                    {
+                        provider: 'VMware',
+                        clusters: 1,
+                        sockets: 164,
+                        vms: 126,
+                        hosts: 2,
+                        product: 'VMware vCenter',
+                        version: '6.5'
+                    }
+                ],
+                complexityModel: { easy: 111, medium: 11, hard: 0, unknown: 0, unsupported: 4 },
+                recommendedTargetsIMSModel: { total: 126, rhv: 122, rhel: 7, osp: 121, ocp: 111 },
+                workloadsDetectedOSTypeModels: [{ osName: 'ServerNT', total: 1 }, { osName: 'Linux', total: 4 }],
+                scanRunModels: [{ target: 'VMware', date: 1580774400000, smartStateEnabled: true }]
+            },
+            reportWorkloadSummaryFetchStatus: { error: null, status: 'complete' },
+            fetchReportWorkloadSummary: (reportId: number) => ({
+                then: (callback: any) => {
+                    callback();
+                    return {
+                        catch: jest.fn()
+                    };
+                }
+            })
+        };
+        const wrapper = shallow(<WorkloadMigrationSummary {...props} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
